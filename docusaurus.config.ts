85386f6d1cf947b15dddcb8c43eb64b6ff163a01
DESKTOP-0TB9585\user <kioj@visang.com>
2024-12-31 17:33:12 +0900

    Configure GitHub Pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,12 +5,12 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'allviaCL VCR Documentation',
+  tagline: 'VCR Documentation',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://vs-softwaredevcell.github.io/',
+  url: 'https://vs-softwaredevcell.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/allviaCL_VCR_doc/',
@@ -19,6 +19,8 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'vs-softwaredevcell', // Usually your GitHub org/user name.
   projectName: 'allviaCL_VCR_doc', // Usually your repo name.
+
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
